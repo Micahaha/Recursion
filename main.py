48ac0a43f333488468e6b29f2b85df37065e4d56
Micahaha <xicer029@gmail.com>
2023-12-08 10:58:31 -0500

    recursion

--- main.py
+++ main.py
@@ -20,14 +20,24 @@ print("2 to the 5th power using a loop is:", recursion.power(2, 5))
 print("2 to the 4th power using a loop is:", recursion.power(2, 4))
 print("2 to the 0 power using a loop is:", recursion.power(2, 0))
 
-list = [4,2,2,56,7]
+list_1 = [4,2,2,56,7]
 i = 0 
-print(f"Min value:{recursion.computeMin(list, 0, list[i])}")
+print(f"Min value:{recursion.computeMin(list_1, 0, list_1[i])}")
 
-list = [4,2,2,56,7]
+list_1 = [4,2,2,56,7]
 i = 0 
-print(f"Min value: {recursion.computeMin(list, 0, list[0])}")
+print(f"Min value: {recursion.computeMin(list_1, 0, list_1[0])}")
 
 str = 'Hello Dog'
 print(f'Reverse String: {recursion.reverse(str, len(str))}')
 
+
+
+print('Enter 7 numbers seperated by a space: ')
+list_of_numbers = list(map(int, input("Enter two numbers separated by space: ").split()))
+index = int(input('Enter the index at which the search will begin: '))
+size = int(input('Enter the size of the list that will be searched: '))
+target = int(input('Enter the target value to search for: '))
+i = 0
+
+print(f'Target found at: {recursion.search(list_of_numbers, index, size, target, i, False)}')
--- recursion.py
+++ recursion.py
@@ -99,9 +99,39 @@ class recursion:
             print(s[i - 1], end="")
             recursion.reverse(s, i-1)
 
-      
 
+    @staticmethod
+    def search (a, first: int, size: int, target, i: int, found: bool):
+        """Searches for a desired element in a list of elements
+        starting at a[first].
 
+        Args:
+            a: the list to search
+            first (int): the list index at which the search will start
+            size (int): the number of elements to search
+            target: the element to search for
+            i: the counter variable used to iterate through list
+            found: the variable used to denote if the target has been found
 
+        Returns:
+            int: If target appears in the list, index of the element
+            that contains the target, else -1.
+        """ 
 
+        # set a boolean variable found to false
+        found = False
+
+        # while there are more elements to search
+        # and the target hasn't been found and
+        # i plus first doesn't exceed the length of
+        # the list
+        if i >= size or (i + first >= len(a)):
+            return -1
+
+        # check if the current element is the target
+        if a[i + first] == target:
+            return i + first
+        else:
+            # recursively search for the target in the rest of the list
+            return recursion.search(a, first, size, target, i + 1, False)
 
